cd604b471c6b9854d444c514faa59a45ba57f054
JesseHartsuiker <jhartsuiker@roc-dev.com>
2019-03-29 09:44:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Opdracht 3</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+$voornaam = 'Jesse';
+$achternaam = 'Hartsuiker';
+$leeftijd = '18';
+$laptop = 'Dell';
+$bijnaweekend = 'Bijna Weekend!!!';
+
+if ($bijnaweekend){
+    echo 'Tijd voor een biertje!!!';
+} else{
+    echo'Nog even wachten tot het weekend is:)';
+}
+ echo "Hallo, ik ben $voornaam $achternaam en ik ben $leeftijd jaar oud en het is $bijnaweekend";
+
+echo 'Hallo, ik ben '.$voornaam.','.$achternaam.' en ik ben '.$leeftijd.' jaar oud ';
+?>
